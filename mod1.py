ca7019e60eb18a11af409522fc8fed117ccc4609
Raquel Braga <87723504+Raquelsantos242@users.noreply.github.com>
2022-07-28 17:39:08 -0300

    Add files via upload

---
+++ mod1.py
@@ -0,0 +1,5 @@
+s = "Total Eclipse of the heart"
+v1 = 999
+
+def f_teste(x):
+    print('x=', x)
---
+++ testa_mod1.py
@@ -0,0 +1,11 @@
+import mod1
+import math
+import statistics as s
+ 
+print(mod1.s)
+print(mod1.v1)
+
+mod1.f_teste(10000)
+
+
+print(s.mean([1, 4, 5, 6, 7]))
