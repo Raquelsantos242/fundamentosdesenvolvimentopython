8acf1ff8f4ecc8b7c9e759f41903a311e937d1f1
Raquel Braga <87723504+Raquelsantos242@users.noreply.github.com>
2022-07-28 17:08:32 -0300

    Add files via upload

---
+++ exemplo_for.py
@@ -0,0 +1,11 @@
+for i in range(3):
+    print(i)
+    
+    print()
+    
+for i in range(1,4):
+    print(i)
+print()  
+for i in range(0, 10, 2):
+        print(i)
+print()
---
+++ exemplo_math.py
@@ -0,0 +1,13 @@
+import math
+
+#constantes PI
+print ('PI = ', math.pi)
+
+#funções de arredondamento
+x1 = 5.9
+print('ceil de pi: ', math.ceil(x1))
+print('floor de pi: ', math.floor(x1))
+print('truncamento de pi: ', math.trunc(x1))
+
+x2 = 1024
+print('log de x2: ', math.log2(x2))
