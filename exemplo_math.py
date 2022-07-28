d67b9ad945898f790c7c511959eabb3a36238712
Raquel Braga <87723504+Raquelsantos242@users.noreply.github.com>
2022-07-28 17:20:47 -0300

    Add files via upload

---
+++ exemplo_math.py
@@ -0,0 +1,13 @@
+import math
+
+#constantes PI
+print ('PI = ', math.pi)
+
+#funções de arredondamento
+x1 = 5.9
+print('ceil de x1: ', math.ceil(x1))
+print('floor de x1: ', math.floor(x1))
+print('truncamento de x1: ', math.trunc(x1))
+
+x2 = 1024
+print('log de x2: ', math.log2(x2))
